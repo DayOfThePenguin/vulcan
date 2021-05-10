947a082b177f478fe0fc6c8814876805bdee3cc0
DayOfThePenguin <colin.dablain@gmail.com>
2021-05-09 19:54:14 -0700

    Added landing page that shows all existing maps

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+"""
+WikiMap: Visualize the connections between ideas
+Copyright (C) 2015  Colin Dablain
+
+    WikiMap is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WikiMap is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WikiMap in COPYING.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import urllib.parse
 import wikipedia
 
@@ -5,6 +23,8 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from pathlib import Path
 
 app = FastAPI()
 
@@ -15,12 +35,18 @@ templates = Jinja2Templates(directory="static/templates")
 def search_result(page_name: str) -> str:
     return(wikipedia.page(page_name))
 
+def get_available_maps():
+    data_path = Path("static/data")
+    available_maps = []
+    for child in data_path.iterdir():
+        if child.suffix == ".json":
+            available_maps.append(child.stem)
+    return available_maps
+
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
-
+    return templates.TemplateResponse("index.html", {"request": request,
+                                                     "maps": get_available_maps()})
 
 @app.get("/maps/{file_name}")
 async def graph_json(file_name: str, request: Request, response_class=HTMLResponse):
