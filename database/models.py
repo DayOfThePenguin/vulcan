7dbd62f24001cc37cbf72502f34786eca880d8c0
DayOfThePenguin <colin.dablain@gmail.com>
2021-06-07 17:54:21 -0700

    cleaned up old files, updated models so lowercase of title is indexed

--- database/models.py
+++ database/models.py
@@ -3,13 +3,22 @@
 Resources
 ---------
 https://www.learndatasci.com/tutorials/using-databases-python-postgres-sqlalchemy-and-alembic/
+https://stackoverflow.com/questions/28829236/is-it-possible-to-ignore-one-single-specific-line-with-pylint
+https://stackoverflow.com/questions/53063778/case-insensitive-indexing-in-postgres-with-sqlalchemy
+https://stackoverflow.com/questions/14419299/adding-indexes-to-sqlalchemy-models-after-table-creation
 """
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Text, String
+from sqlalchemy.ext.hybrid import Comparator, hybrid_property
+from sqlalchemy import func, Column, Index, Text, String
 from sqlalchemy.dialects.postgresql import ARRAY
 
 Base = declarative_base()
+
+
+class CaseInsensitiveComparator(Comparator):
+    def __eq__(self, other):
+        return func.lower(self.__clause_element__()) == func.lower(other)
 
 
 class Page(Base):
@@ -19,6 +28,14 @@ class Page(Base):
     sections = Column(ARRAY(Text()))
     links = Column(ARRAY(String(200)))
 
+    @hybrid_property
+    def title_insensitive(self):
+        return self.title.lower()
+
+    @title_insensitive.comparator
+    def title_insensitive(cls):  # pylint: disable=no-self-argument
+        return CaseInsensitiveComparator(cls.title)
+
     def __repr__(self):
         msg = "<Page: (\n\t"
         msg += "title='{}',\n\t".format(self.title)
@@ -27,3 +44,13 @@ class Page(Base):
         msg += "links[:10]={}\n".format(self.links[:10])
         msg += ")>"
         return msg
+
+
+title_index = Index("title_index", func.lower(Page.title))
+
+if __name__ == "__main__":
+    from database.config import get_engine
+
+    DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/complete_wikipedia"
+    engine = get_engine(DATABASE_URI)
+    title_index.create(bind=engine)
---
+++ scoping.md
@@ -0,0 +1,4 @@
+to compare every page to every other page, you need to calculate (and store?) cosine similarities 3.89e13 times (len wikipedia ^ 2)
+
+pytorch cos sim for https://stackoverflow.com/questions/50411191/how-to-compute-the-cosine-similarity-in-pytorch-for-all-rows-in-a-matrix-with-re
+pytorch free gpu memory: https://stackoverflow.com/questions/55788093/how-to-free-gpu-memory-by-deleting-tensors
